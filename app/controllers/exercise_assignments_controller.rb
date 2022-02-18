f71744eabb590b704ccaf6eaad0e2ba78350bda7
Kyle Shipley <kshipley@hellobrightline.com>
2022-02-17 22:22:43 -0500

    Set up a basic placeholder exercise_assignments view

--- app/controllers/exercise_assignments_controller.rb
+++ app/controllers/exercise_assignments_controller.rb
@@ -4,20 +4,34 @@ class ExerciseAssignmentsController < ApplicationController
   def index
     @exercise_assignments = ExerciseAssignment.all
 
-    render json: @exercise_assignments
+    respond_to do |format|
+      format.html
+      format.json { render json: @exercise_assignments }
+    end
   end
 
   def show
     render json: @exercise_assignment
   end
 
+  def new
+    @exercise_assignment = ExerciseAssignment.new
+  end
+
   def create
     @exercise_assignment = ExerciseAssignment.new(exercise_assignment_params)
 
     if @exercise_assignment.save
-      render json: @exercise_assignment, status: :created, location: @exercise_assignment
+      respond_to do |format|
+        format.html { redirect_to @exercise_assignment }
+        format.json { render json: @exercise_assignment, status: :created, location: @exercise_assignment }
+      end
     else
-      render json: @exercise_assignment.errors, status: :unprocessable_entity
+      respond_to do |format|
+        format.html { render :new }
+        format.json { render json: @exercise_assignment.errors, status: :unprocessable_entity }
+      end
+
     end
   end
 
