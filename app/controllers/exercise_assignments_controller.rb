09d1f7fd709ba71b0e5555a973c9a56850cfac12
Kyle Shipley <kshipley@hellobrightline.com>
2022-02-26 15:35:46 -0500

    Remove parts of code we might want the candidate to write

--- app/controllers/exercise_assignments_controller.rb
+++ app/controllers/exercise_assignments_controller.rb
@@ -10,43 +10,6 @@ class ExerciseAssignmentsController < ApplicationController
     end
   end
 
-  def show
-    render json: @exercise_assignment
-  end
-
-  def new
-    @exercise_assignment = ExerciseAssignment.new
-  end
-
-  def create
-    @exercise_assignment = ExerciseAssignment.new(exercise_assignment_params)
-
-    if @exercise_assignment.save
-      respond_to do |format|
-        format.html { redirect_to @exercise_assignment }
-        format.json { render json: @exercise_assignment, status: :created, location: @exercise_assignment }
-      end
-    else
-      respond_to do |format|
-        format.html { render :new }
-        format.json { render json: @exercise_assignment.errors, status: :unprocessable_entity }
-      end
-
-    end
-  end
-
-  def update
-    if @exercise_assignment.update(exercise_assignment_params)
-      render json: @exercise_assignment
-    else
-      render json: @exercise_assignment.errors, status: :unprocessable_entity
-    end
-  end
-
-  def destroy
-    @exercise_assignment.destroy
-  end
-
   private
     def set_exercise_assignment
       @exercise_assignment = ExerciseAssignment.find(params[:id])
